60a0f9cd4f1c8f0d281da7593d538087ec79d3fe
Habib Erfani <94376141+habibErfani@users.noreply.github.com>
2022-05-12 09:54:14 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -47,6 +47,5 @@ $router ->get('/items', [new ItemsController($container->get('db_connection')), 
 $router ->post('/items', [new ItemPostController($container->get('db_connection')), 'ajoutItems']);
 $router ->delete('/items', [new ItemsController($container->get('db_connection')), 'delete']);
 
-
 $response = $router->dispatch($request);
 (new SapiEmitter())->emit($response);
