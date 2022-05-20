f94e31e2419956028a415b2c293032a50516ef43
Habib Erfani <94376141+habibErfani@users.noreply.github.com>
2022-05-20 09:10:36 +0200

    Update Home.php

--- public/Controllers/Home.php
+++ public/Controllers/Home.php
@@ -7,7 +7,7 @@ use Laminas\Diactoros\Response\HtmlResponse;
 class Home
 {  
     public function home(): HtmlResponse
-    {
+    { 
         return new HtmlResponse('
 <h1><div style=\"text-align: center;\">Bienvenue dans mon API</div></h1>
         <nav>
