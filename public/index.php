bfb64af6c69cfa3003694b6507f7edb1f37402cf
Habib Erfani <94376141+habibErfani@users.noreply.github.com>
2022-06-06 12:47:10 +0200

    Create index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ include '../vendor/autoload.php';
 
 use App\Controllers\ItemPostController;
 use App\Controllers\ItemsController;
-  
+ 
 use Doctrine\DBAL\DriverManager;
 use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
